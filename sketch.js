2bd05c35b010022199dbbb1103f9e80aeec8ea35
aditirshenai <60028245+aditirshenai@users.noreply.github.com>
2020-03-26 18:34:00 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,76 @@
+var Engine = Matter.Engine,
+    World = Matter.World,
+    Bodies = Matter.Bodies;
+ 
+var engine;
+var world;
+var boxes = [];
+ 
+var ground;
+var gSlider;
+ 
+ 
+function setup() {
+    createCanvas(800, 800);
+    engine = Engine.create();
+    world = engine.world;
+    Engine.run(engine);
+ 
+    gSlider = createSlider(0, 200, 100);
+    gSlider.position(80, 730);
+    gSlider.input = map(engine.world.gravity, gSlider.min, gSlider.max, 0, 1);
+ 
+    var options = {
+        isStatic: true
+    }
+    ground = Bodies.rectangle(200, height - 50, width, 10, options);
+    World.add(world, ground);
+}
+ 
+function mousePressed() {
+    if (mouseY < 350) {
+        boxes.push(new Box(mouseX, mouseY, random(10, 40), random(10, 40)));
+    }
+}
+ 
+function draw() {
+    background(255);
+    var fVal = gSlider.value();
+ 
+    for (var i = 0; i < boxes.length; i++) {
+        boxes[i].show();
+    }
+    noStroke();
+    fill(170);
+    strokeWeight(4);
+    rectMode(CENTER);
+    rect(ground.position.x, ground.position.y, width, 10);
+    fill(255);
+}
+ 
+function Box(x, y, w, h, options) {
+    var options = {
+        friction: 2.0,
+        restitution: 2,
+    }
+ 
+    this.body = Bodies.rectangle(x, y, w, h, options);
+    this.w = w;
+    this.h = h;
+    World.add(world, this.body);
+ 
+    this.show = function () {
+        var pos = this.body.position;
+        var angle = this.body.angle;
+ 
+        push();
+        translate(pos.x, pos.y);
+        rotate(angle);
+        rectMode(CENTER);
+        strokeWeight(1);
+        stroke(156);
+        fill(127);
+        rect(0, 0, this.w, this.h);
+        pop();
+    }
+}
